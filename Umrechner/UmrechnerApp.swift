d8589d84f05fbf076b1a6d4df7bea00bf3a68bd0
fuessner <94464896+fuessner@users.noreply.github.com>
2026-03-18 18:13:28 +0100

    Initial Commit

---
+++ Umrechner/UmrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmrechnerApp.swift
+//  Umrechner
+//
+//  Created by Rolf Füßner on 18.03.26.
+//
+
+import SwiftUI
+
+@main
+struct UmrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
