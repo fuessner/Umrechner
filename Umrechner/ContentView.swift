d4f3a2403f8db9592b3951dc784704b3899ba4f1
fuessner <94464896+fuessner@users.noreply.github.com>
2026-03-30 19:33:16 +0200

    Umrechnung

--- Umrechner/ContentView.swift
+++ Umrechner/ContentView.swift
@@ -8,25 +8,37 @@
 // APP die Unterstützen kann: KUNISWY
 
 // https://www.youtube.com/watch?v=XQN9InGR_o8
-// Zeit: 2:35
+// Zeit: 3:14
 // Aktuell Funktionen
 // --------------------------------------------
 
 import SwiftUI
 
 struct ContentView: View {
-    @State var anzahl: Int = 0
+
+    @State var kilometer: Double = 1.0
+    let kilometerInMeilenFaktor: Double = 0.621371
+    
     var body: some View {
-        VStack(spacing: 10){
-            Text("Anzahl: \(anzahl)")
-            Button("Hochzählen"){
-                anzahl += 1
-            }
+        
+        VStack{
+            Slider(value: $kilometer, in: 0...100)
+                .padding(.horizontal, 100)
+            // Text("Kilometer: " + (String(format: "%.3f", kilometer).replacingOccurrences(of: ".", with: ",")))
             
-            // Test
+            // Text("Kilometer: " + doubleToString(value: kilometer))
+            Text("Kilometer: " + doubleToString(kilometer))
+            Text("In Meilen: " + doubleToString(kilometer * kilometerInMeilenFaktor))
         }
 
         
+        
+    }
+    // den "_ value" mache ich aus dem grund, damit ich beim aufruf das wort value nicht mitgeben muss
+    // siehe oben beim aufruf
+    func doubleToString(_ value: Double) -> String {
+        let text = String(format: "%.3f", value)
+        return text.replacingOccurrences(of: ".", with: ",")
         
     }
 }
