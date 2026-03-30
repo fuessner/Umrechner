741aab32f4d17b09c87342332012c34b77f3e282
fuessner <94464896+fuessner@users.noreply.github.com>
2026-03-30 18:14:30 +0200

    Test

--- Umrechner/ContentView.swift
+++ Umrechner/ContentView.swift
@@ -4,18 +4,30 @@
 //
 //  Created by Rolf Füßner on 18.03.26.
 //
+// --------------------------------------------
+// APP die Unterstützen kann: KUNISWY
+
+// https://www.youtube.com/watch?v=XQN9InGR_o8
+// Zeit: 2:35
+// Aktuell Funktionen
+// --------------------------------------------
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var anzahl: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10){
+            Text("Anzahl: \(anzahl)")
+            Button("Hochzählen"){
+                anzahl += 1
+            }
+            
+            // Test
         }
-        .padding()
+
+        
+        
     }
 }
 
